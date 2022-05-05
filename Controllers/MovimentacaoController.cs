ab40df63f75a78b7c2f863470f712d64290f0eaf
Rafael <rafaeltavares462@gmail.com>
2022-05-04 21:05:32 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/MovimentacaoController.cs
@@ -0,0 +1,65 @@
+﻿using Financas.DAO;
+using Financas.Entidades;
+using Financas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    [Authorize]
+    public class MovimentacaoController : Controller
+    {
+        // GET: Movimentacao
+        private MovimentacaoDAO movimentacaoDAO;
+        private UsuarioDAO usuarioDAO;
+
+        public MovimentacaoController(MovimentacaoDAO movimentacaoDAO, UsuarioDAO usuarioDAO)
+        {
+            this.movimentacaoDAO = movimentacaoDAO;
+            this.usuarioDAO = usuarioDAO;
+        }
+
+        public ActionResult Index()
+        {
+            return View(movimentacaoDAO.Lista());
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Usuarios = usuarioDAO.Lista();
+            return View();
+        }
+
+        public ActionResult Adiciona(Movimentacao movimentacao)
+        {
+            if (ModelState.IsValid)
+            {
+                movimentacaoDAO.Adiciona(movimentacao);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Usuarios = usuarioDAO.Lista();
+                return View("Form");
+            }
+        }
+        public ActionResult MovimentacoesPorUsuario(MovimentacoesPorUsuarioModel model)  // A view foi criada apartir desse metodo
+        {
+            model.Usuarios = usuarioDAO.Lista(); // está listando os usuarios do banco de dados
+            model.Movimentacoes = movimentacaoDAO.BuscaPorUsuario(model.UsuarioId);
+            return View(model);
+        }
+
+        public ActionResult Busca(BuscaMovimentacoesModel model) // Aview foi criada apartir desse metodo 
+        {
+            model.Usuarios = usuarioDAO.Lista();
+            model.Movimentacoes = movimentacaoDAO.Busca(model.ValorMinimo, model.ValorMaximo, // preenchendo a lista da movimentaçoes com os parametros
+                                    model.DataMinima, model.DataMaxima,
+                                    model.Tipo, model.UsuarioId);
+            return View(model);
+        }
+    }
+}
---
+++ DAO/MovimentacaoDAO.cs
@@ -0,0 +1,65 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class MovimentacaoDAO
+    {
+        private FinancasContext context;
+        public MovimentacaoDAO(FinancasContext context)
+        {
+            this.context = context;
+        }
+        public void Adiciona(Movimentacao movimentacao)
+        {
+            context.Movimentacoes.Add(movimentacao);
+            context.SaveChanges();
+        }
+        public IList<Movimentacao> Lista()
+        {
+            return context.Movimentacoes.ToList();
+        }
+
+
+
+        public IList<Movimentacao> BuscaPorUsuario(int? usuarioID)
+        {
+            return context.Movimentacoes.Where(m => m.UsuarioId == usuarioID).ToList();
+        }
+
+        public IList<Movimentacao> Busca(decimal? valorMinimo, decimal? valorMaximo,
+                                        DateTime? dataMinima, DateTime? dataMaxima,
+                                        Tipo? tipo, int? usuario)
+        {
+            IQueryable<Movimentacao> resultado = context.Movimentacoes; // Nessa condicional está checando se os valores foram preenchidos para fazer a busca
+            if (valorMinimo.HasValue)
+            {
+                resultado = resultado.Where(m => m.Valor >= valorMinimo);
+            }
+            if (valorMaximo.HasValue)
+            {
+                resultado = resultado.Where(m => m.Valor <= valorMaximo);
+            }
+            if (dataMinima.HasValue)
+            {
+                resultado = resultado.Where(m => m.Data >= dataMinima);
+            }
+            if (dataMaxima.HasValue)
+            {
+                resultado = resultado.Where(m => m.Data <= dataMaxima);
+            }
+            if (tipo.HasValue)
+            {
+                resultado = resultado.Where(m => m.Tipo == tipo);
+            }
+            if (usuario.HasValue)
+            {
+                resultado = resultado.Where(m => m.UsuarioId == usuario);
+            }
+            return resultado.ToList();  // Quando os resultado for encontrado ai sim os valores são listados
+        }
+    }
+}
